7d32ca07eb6d3b9baed91d2475ba97c4bc5f7f4e
Brandon Olin <brandon@devblackops.io>
2018-11-07 22:44:48 -0800

    refactory build

--- build.settings.ps1
+++ build.settings.ps1
@@ -1,10 +1,15 @@
-$projectRoot = if ($ENV:BHProjectPath) { $ENV:BHProjectPath } else { $PSScriptRoot }
-
+$projectRoot    = if ($ENV:BHProjectPath) { $ENV:BHProjectPath } else { $PSScriptRoot }
+$moduleName     = $env:BHProjectName
+$moduleVersion  = (Import-PowerShellDataFile -Path $env:BHPSModuleManifest).ModuleVersion
+$outDir         = Join-Path -Path $projectRoot -ChildPath Output
+$moduleOutDir   = "$outDir/$moduleName/$moduleVersion"
 @{
     ProjectRoot     = $projectRoot
     ProjectName     = $env:BHProjectName
     SUT             = $env:BHModulePath
-    Tests           = Join-Path -Path $projectRoot -ChildPath Tests
+    Tests           = Join-Path -Path $projectRoot -ChildPath tests
+    OutputDir       = $outDir
+    ModuleOutDir    = $moduleOutDir
     ManifestPath    = $env:BHPSModuleManifest
     Manifest        = Import-PowerShellDataFile -Path $env:BHPSModuleManifest
     PSVersion       = $PSVersionTable.PSVersion.ToString()
--- psakeFile.ps1
+++ psakeFile.ps1
@@ -12,9 +12,9 @@ task Init {
 
 task Test -Depends Init, Analyze, Pester -description 'Run test suite'
 
-task Analyze -depends Init {
-    $analysis = Invoke-ScriptAnalyzer -Path $settings.SUT -Recurse -Verbose:$false
-    $errors = $analysis | Where-Object {$_.Severity -eq 'Error'}
+task Analyze -depends Build {
+    $analysis = Invoke-ScriptAnalyzer -Path $settings.ModuleOutDir -Recurse -Verbose:$false
+    $errors   = $analysis | Where-Object {$_.Severity -eq 'Error'}
     $warnings = $analysis | Where-Object {$_.Severity -eq 'Warning'}
     if (@($errors).Count -gt 0) {
         Write-Error -Message 'One or more Script Analyzer errors were found. Build cannot continue!'
@@ -27,18 +27,36 @@ task Analyze -depends Init {
     }
 } -description 'Run PSScriptAnalyzer'
 
-task Pester -depends Init {
+task Pester -depends Build {
     Remove-Module $settings.ProjectName -ErrorAction SilentlyContinue -Verbose:$false
-    Import-Module -Name $settings.ManifestPath -Force -Verbose:$false
 
-    if (Test-Path -Path $settings.Tests) {
-        Invoke-Pester -Path $settings.Tests -PassThru -EnableExit
+    $testResultsXml = Join-Path -Path $settings.OutputDir -ChildPath 'testResults.xml'
+    $testResults = Invoke-Pester -Path $settings.Tests -PassThru -OutputFile $testResultsXml -OutputFormat NUnitXml
+
+    # Upload test artifacts to AppVeyor
+    if ($env:APPVEYOR_JOB_ID) {
+        $wc = New-Object 'System.Net.WebClient'
+        $wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", $testResultsXml)
+    }
+
+    if ($testResults.FailedCount -gt 0) {
+        $testResults | Format-List
+        Write-Error -Message 'One or more Pester tests failed. Build cannot continue!'
     }
 } -description 'Run Pester tests'
 
-task Build -depends Test {}
+task Clean -depends Init {
+    if (Test-Path -Path $settings.ModuleOutDir) {
+        Remove-Item -Path $settings.ModuleOutDir -Recurse -Force -Verbose:$false
+    }
+}
 
-task Publish -depends Init {
+task Build -depends Init, Clean {
+    New-Item -Path $settings.ModuleOutDir -ItemType Directory -Force > $null
+    Copy-Item -Path "$($settings.SUT)/*" -Destination $settings.ModuleOutDir -Recurse
+}
+
+task Publish -depends Test {
     "    Publishing version [$($settings.Manifest.ModuleVersion)] to PSGallery..."
     if ($settings.PSGalleryApiKey) {
         Publish-Module -Path $settings.SUT -NuGetApiKey $settings.PSGalleryApiKey -Repository PSGallery
